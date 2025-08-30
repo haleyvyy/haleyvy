42784fef180c3a11c92a046a266a290eb054d5b3
Julie <juliereyes0126@gmail.com>
2025-08-29 19:40:36 -0700

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My GitHub Homepage</h1>
+      <p>This is the Home Page. Navigate using the menu.</p>
+    </div>
+  );
+}
